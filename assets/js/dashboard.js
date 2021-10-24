b434160b1cb523a6ab78faf65027550a92025d9f
alirezaImani-f4L3e <83541857+alirezaImani-f4L3e@users.noreply.github.com>
2021-10-25 00:09:36 +0330

    adding dashboard ,files and add file

---
+++ assets/js/dashboard.js
@@ -0,0 +1,11 @@
+var elem = document.getElementById("demo");
+
+function profile() {
+    console.log(event);
+    elem.style.display = "inline";
+    elem.style.top = "25px";
+}
+
+function out() {
+    elem.style.display = "none";
+}
